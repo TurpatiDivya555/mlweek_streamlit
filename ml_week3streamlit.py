105fa6835cdbd9179b0e6a9af03fffb86cc330c5
TurpatiDivya555 <turpatidivya.22.csm@anits.edu.in>
2024-09-03 20:55:17 +0530

    Update ml_week3streamlit.py

--- ml_week3streamlit.py
+++ ml_week3streamlit.py
@@ -14,10 +14,10 @@ clf.fit(X, y)
 
 # Define a function to take user input for the Iris flower features
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length (cm)',  	float(X[:,0].min()), float(X[:,0].max()), float(X[:,0].mean()))
-    sepal_width = st.sidebar.slider('Sepal width (cm)',  	float(X[:,1].min()), float(X[:,1].max()), float(X[:,1].mean()))
-    petal_length = st.sidebar.slider('Petal length (cm)',  	float(X[:,2].min()), float(X[:,2].max()), float(X[:,2].mean()))
-    petal_width = st.sidebar.slider('Petal width (cm)',  	float(X[:,3].min()), float(X[:,3].max()), float(X[:,3].mean()))
+    sepal_length = st.sidebar.slider('Sepal length (cm)', float(X[:, 0].min()), float(X[:, 0].max()), float(X[:, 0].mean()))
+    sepal_width = st.sidebar.slider('Sepal width (cm)', float(X[:, 1].min()), float(X[:, 1].max()), float(X[:, 1].mean()))
+    petal_length = st.sidebar.slider('Petal length (cm)', float(X[:, 2].min()), float(X[:, 2].max()), float(X[:, 2].mean()))
+    petal_width = st.sidebar.slider('Petal width (cm)', float(X[:, 3].min()), float(X[:, 3].max()), float(X[:, 3].mean()))
     
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
@@ -26,6 +26,7 @@ def user_input_features():
     
     features = pd.DataFrame(data, index=[0])
     return features
+
 # Get user input
 df = user_input_features()
 
@@ -40,5 +41,6 @@ prediction_proba = clf.predict_proba(df)
 # Display the prediction and corresponding probability
 st.subheader('Prediction')
 st.write(iris.target_names[prediction][0])
+
 st.subheader('Prediction Probability')
 st.write(pd.DataFrame(prediction_proba, columns=iris.target_names))
